8a31112f85bf698d17a080e6bdd9e75caddea95f
DaoriChan <dadirdev@gmail.com>
2025-07-31 14:37:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-import asyncio
-from playwright.async_api import async_playwright
+import time
 
-async def visit_site():
-    print("Запуск Playwright...")
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        try:
-            await page.goto("https://cerebrysquad.github.io/")
-            title = await page.title()
-            print(f"Сайт загружен\nЗаголовок страницы: {title}")
-        except Exception as e:
-            print("Ошибка:", e)
-        await browser.close()
+print("Скрипт стартовал...")
 
-async def keep_alive():
-    while True:
-        print("Бот работает...")
-        await asyncio.sleep(5)
-
-async def main():
-    await visit_site()
-    await keep_alive()
-
-asyncio.run(main())
+while True:
+    print("Я всё ещё работаю...")
+    time.sleep(5)
