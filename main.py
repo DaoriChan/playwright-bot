7e2ec7698dfd31be02a666d2f90677f99159c3bc
DaoriChan <dadirdev@gmail.com>
2025-07-31 14:54:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("=== Script started ===")
+import asyncio
+from playwright.async_api import async_playwright
+
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch(headless=True)  # headless=False для видимости
+        page = await browser.new_page()
+        await page.goto("https://cerebrysquad.xyz")
+        print("Opened cerebrysquad!")
+
+        while True:
+            print("still alive")
+            await asyncio.sleep(5)
+
+asyncio.run(main())
